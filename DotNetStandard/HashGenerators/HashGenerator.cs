c19cbbcd74dd15e5abcad91069c623362b81fbcd
Keith La Force <klaforce@skookum.com>
2013-03-20 09:43:26 -0400

    Updating the project to use semantic versioning and adding hash generator to the standard library.

---
+++ DotNetStandard/HashGenerators/HashGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetStandard.HashGenerators
+{
+    public static class HashGenerator
+    {
+        public static string GetRandomHash()
+        {
+            Random r = new Random();
+            return GetHash(r.Next(Int32.MaxValue).ToString(CultureInfo.InvariantCulture));
+        }
+
+        public static string GetHash(string text)
+        {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] message = UE.GetBytes(text);
+
+            SHA256Managed hashString = new SHA256Managed();
+            StringBuilder hex = new StringBuilder();
+
+            byte[] hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hex.Append(String.Format("{0:x2}", x));
+            }
+            return hex.ToString();
+        }
+
+        public static string GetBCryptHash(string text)
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword(text);
+
+            return hash;
+        }
+    }
+}
