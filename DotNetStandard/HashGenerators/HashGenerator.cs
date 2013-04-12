f5b04417f235f3b2c20d5852e86fa07ff1206344
Keith La Force <klaforce@skookum.com>
2013-04-12 15:02:02 -0400

    Add new MD5 hash function.

--- DotNetStandard/HashGenerators/HashGenerator.cs
+++ DotNetStandard/HashGenerators/HashGenerator.cs
@@ -38,5 +38,24 @@ namespace DotNetStandard.HashGenerators
 
             return hash;
         }
+
+        public static string GetMd5Hash(string input)
+        {
+            var data = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes 
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data  
+            // and format each one as a hexadecimal string. 
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string. 
+            return sBuilder.ToString();
+        }
     }
 }
