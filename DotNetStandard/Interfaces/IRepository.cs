e0da6ea424e6ab99fcc5816fc94444ae5e1a6758
Patrick Faulkner <pfaulkner@skookum.com>
2014-02-24 14:11:54 -0500

    Add IRepository to Standard project

---
+++ DotNetStandard/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetStandard.Interfaces
+{
+    public interface IRepository
+    {
+        IQueryable<T> Get<T>() where T : class;
+
+        void Create<T>(T item) where T : class;
+        void Create<T>(IEnumerable<T> items) where T : class;
+
+        void Update<T>(T item) where T : class;
+        void Update<T>(IEnumerable<T> items) where T : class;
+
+        void Delete<T>(T item) where T : class;
+        void Delete<T>(IEnumerable<T> items) where T : class;
+
+        void Refresh<T>(T item) where T : class;
+    }
+}
